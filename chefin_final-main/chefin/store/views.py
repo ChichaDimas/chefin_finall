770397b2aa784983d840593687b4274d1e195a74
vilka <vilkamil9@gmail.com>
2023-07-04 21:42:51 +0300

    vadik rabotay suka

--- chefin_final-main/chefin/store/views.py
+++ chefin_final-main/chefin/store/views.py
@@ -186,7 +186,7 @@ def basket_add(request, product_id):
     name = data.get('name')
     image = data.get('image')
     description = data.get('description')
-    print(price)
+    print(description)
 
     # Создание JSON-представления товара с комментарием и постером
     product_json = {
